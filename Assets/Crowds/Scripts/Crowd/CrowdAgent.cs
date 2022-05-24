d41b2234b3d9000b994b79a2992075cab5d10baf
3kije <martahyjek043@gmail.com>
2022-05-24 13:25:21 +0200

    Initial commit

---
+++ Assets/Crowds/Scripts/Crowd/CrowdAgent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
+public class CrowdAgent : MonoBehaviour {
+       
+    [SerializeField]
+    private Transform target;
+    private GameObject animator;
+
+    private UnityEngine.AI.NavMeshAgent agent;
+
+	void Start () {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.speed = Random.Range(4.0f, 5.0f);
+        agent.SetDestination(target.position);
+        animator.GetComponent<Animator>();
+	}
+    private void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/PathFollowing/Scripts/PathFollowing/Pathing.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+
+public class Pathing : MonoBehaviour 
+{
+    [SerializeField]
+    private Path path;
+    [SerializeField]
+    private float speed = 20.0f;
+    [SerializeField]
+    private float mass = 5.0f;
+    [SerializeField]
+    private bool isLooping = true;
+    
+    private float currentSpeed;
+    private int currentPathIndex = 0;
+    private Vector3 targetPoint;
+    private Vector3 direction;
+    private Vector3 targetDirection;  
+
+	private void Start () 
+    {        
+        direction = transform.forward;         
+        targetPoint = path.GetPoint(currentPathIndex);
+	}
+
+    private void Update()
+    {
+        if (path == null)
+        {
+            return;
+        }
+
+        currentSpeed = speed * Time.deltaTime;
+
+        if (TargetReached())
+        {
+            if (!SetNextTarget())
+            {
+                return;
+            }
+        }
+
+        direction += Steer(targetPoint);
+        transform.position += direction;
+        transform.rotation = Quaternion.LookRotation(direction);
+
+    }
+    private bool SetNextTarget() 
+    {
+        bool success = false;
+        if (currentPathIndex < path.PathLength - 1) {
+            currentPathIndex++;
+            success = true;
+        } 
+        else 
+        {
+            if(isLooping) 
+            {
+                currentPathIndex = 0;
+                success = true;
+            } 
+            else 
+            {
+                success = false;
+            }
+        }
+        targetPoint = path.GetPoint(currentPathIndex);
+        return success;
+    }
+
+    private bool TargetReached() 
+    {
+        return (Vector3.Distance(transform.position, targetPoint) < path.radius);
+    }
+
+  
+    public Vector3 Steer(Vector3 target)
+    { 
+        targetDirection = (target - transform.position);
+        targetDirection.Normalize();        
+        targetDirection*= currentSpeed;
+		
+        Vector3 steeringForce = targetDirection - direction; 
+        Vector3 acceleration = steeringForce / mass;
+
+        return acceleration;
+    }
+}
